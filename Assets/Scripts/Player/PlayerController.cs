a6314a53e6b61d5983f695cdd32873af315bd571
Serj Qazi <sirajum.qazi@gmail.com>
2025-07-30 20:30:35 -0400

    Add sprite flipping to PlayerController
    Introduced a SpriteFlip method to flip the player sprite based on horizontal input. Also initialized Rigidbody2D and SpriteRenderer components in Start().

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Rendering.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,7 +9,8 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component attached to the GameObject
+        sr = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component attached to the GameObject
     }
 
     // Update is called once per frame
@@ -21,9 +21,38 @@ public class PlayerController : MonoBehaviour
         float hValue = Input.GetAxisRaw("Horizontal");
         rb.linearVelocityX = hValue * 5f;
 
-        if (Input.GetButtonDown("Jump")) 
+        SpriteFlip(hValue); // Call the method to flip the sprite based on horizontal input
+
+        if (Input.GetButtonDown("Jump"))
         {
             rb.AddForce(Vector2.up * 8f, ForceMode2D.Impulse); // AddForce applies a force to the Rigidbody2D
         }
+    }
+
+    void SpriteFlip(float hValue)
+    {
+        // Flip the sprite based on the horizontal input
+        //if (hValue < 0)
+        //{
+        //    sr.flipX = true; // Flip the sprite to face left
+        //}
+        //else if (hValue > 0)
+        //{
+        //    sr.flipX = false; // Flip the sprite to face right
+        //}
+
+        // Simplified sprite flipping logic
+        //sr.flipX = hValue < 0;
+        //if (hValue != 0 ) sr.flipX = hValue < 0; // Flip the sprite based on horizontal input
+
+        // Better, optimized sprite flipping logic
+        // If the sprite is flipped and the horizontal input is in the opposite direction, flip it back
+        if (hValue > 0 && sr.flipX || hValue < 0 && !sr.flipX)
+        {
+            sr.flipX = !sr.flipX;
+
+
+        }
+
     }
 }
